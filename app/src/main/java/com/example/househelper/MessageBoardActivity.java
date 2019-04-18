d8010c576c89996659b6e51b714b2846f38a2da2
davisfaherty <davisfaherty@berkeley.edu>
2019-04-18 11:30:48 -0700

    First commit - laid out files

---
+++ app/src/main/java/com/example/househelper/MessageBoardActivity.java
@@ -0,0 +1,13 @@
+package com.example.househelper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MessageBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message_board);
+    }
+}
---
+++ app/src/main/java/com/example/househelper/SupplyListActivity.java
@@ -0,0 +1,13 @@
+package com.example.househelper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SupplyListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_supply_list);
+    }
+}
