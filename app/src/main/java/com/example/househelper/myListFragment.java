28cf7fa37b8a0da81ed1066fc5d63de1bcf2c768
Caitlin <clee1228@berkeley.edu>
2019-05-01 13:28:43 -0700

    profile list view

--- app/src/main/java/com/example/househelper/myListFragment.java
+++ app/src/main/java/com/example/househelper/myListFragment.java
@@ -1,21 +1,28 @@
 package com.example.househelper;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v4.app.ListFragment;
+import android.util.Log;
 import android.widget.AdapterView;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class myListFragment extends ListFragment implements AdapterView.OnItemClickListener {
+
 
     @Override
     public View onCreateView(LayoutInflater inflater,
@@ -35,6 +42,71 @@ public class myListFragment extends ListFragment implements AdapterView.OnItemCl
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Toast.makeText(getActivity(), "Item: " + position, Toast.LENGTH_SHORT).show();
+        switch(position) {
+            case 0:
+
+
+                AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
+                alertDialog.setTitle("Change Password");
+                final EditText currPw = new EditText(getActivity());
+                LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                        LinearLayout.LayoutParams.WRAP_CONTENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT,
+                        LinearLayout.VERTICAL);
+                currPw.setLayoutParams(lp);
+                currPw.setHint("Enter current password");
+                alertDialog.setView(currPw);
+
+                final EditText newPw = new EditText(getActivity());
+                currPw.setLayoutParams(lp);
+                currPw.setHint("Enter new password");
+                alertDialog.setView(newPw);
+
+
+                alertDialog.setPositiveButton("Change Password",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                String password = currPw.getText().toString();
+                                        Toast.makeText(getActivity(),
+                                                password, Toast.LENGTH_SHORT).show();
+
+
+                            }
+                        });
+
+                alertDialog.setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+
+                alertDialog.show();
+                break;
+            case 1:
+                Toast.makeText(getActivity(), "email", Toast.LENGTH_SHORT).show();
+                break;
+            case 2:
+                Toast.makeText(getActivity(), "password", Toast.LENGTH_SHORT).show();
+                break;
+            case 3:
+                signOut();
+                break;
+        }
+
+    }
+
+
+
+
+
+
+
+    public void signOut(){
+        FirebaseAuth.getInstance().signOut();
+        Toast.makeText(getActivity(), "Signed out", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(getActivity(), LoginActivity.class);
+        startActivity(intent);
+
     }
 }
